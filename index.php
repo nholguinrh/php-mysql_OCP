dd92de96f0ff76e9e65754b88ffbacb66ada8bbf
Nelson Holguin <110838453+nholguinrh@users.noreply.github.com>
2022-11-23 14:32:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,20 @@ require_once("./login_db.php");
 
 if(isset($_POST))
 	{
-		$sql = 'INSERT INTO `sampledb`.`User` (`name`) VALUES ("'.$name.'");';
-
-		if ($conn->query($query) === TRUE) {
-			echo "User created successfully";
-		} else {
-			echo "Error: <br>" . $conn->error;
-		}
+		$sql = 'INSERT INTO `php-mysql_OCP`.`User` (`name`) VALUES ("'.$_POST["name"].'");';
+		if ($mysqli->query($query) === TRUE) 
+			echo "User created successfully <br>"; 
+		else
+			echo "Error: <br>" . $conn->error."<br>";
 	}
 
 echo '
-
-	<h3>Add New User</h3>
 	<form action="index.php" method="post">
 		Name: <input type="text" id="name" name="name"><br />
-		<input type = "submit" onclick="validateUserInfo()">
+		<input type = "submit">
 	</form>';
+
+$nombre=$mysqli->query("SELECT * FROM `php-mysql_OCP`.`User`");
+for($i=0;$i<count($nombre,0);$i++)
+	echo $i." : ".$nombre[$i][0].".<br>";
 ?>
